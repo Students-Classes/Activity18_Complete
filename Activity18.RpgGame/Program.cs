f179a6d0cd8adc1bbb7464009c93d8e5db31b6c5
genisgilavert <anonymousheimer@gmail.com>
2024-02-21 17:16:37 +0100

    Add project files.

---
+++ Activity18.RpgGame/Program.cs
@@ -0,0 +1,7 @@
+﻿using Activity18.RpgGame.Startup;
+
+// TODO: Implementar Inyección de Dependencias en esta aplicación de consola siguiendo las instrucciones de la siguiente guía: https://cord-lip-18a.notion.site/Implementaci-n-de-Inyecci-n-de-Dependencias-en-una-aplicaci-n-de-Consola-en-NET-fa56ff02288841339f8fc959402bcff8?pvs=4
+
+// TODO: El registro de Servicios no se hara aquí, se hará en un método estático de la clase ServiceCollectionExtensions que se encuentra en el proyecto Activity18.RpgGame/Startup/ServiceCollectionExtensions.cs
+
+// TODO: Una vez hayas hecho la implementación de la inyección de dependencias, deberás de obtener una instancia de la IRpgGame y llamar al método Start() para que la aplicación funcione correctamente.
---
+++ Activity18.RpgGame/Startup/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// TODO: necesitaras acceder al siguiente namespace para registrar los servicios: using Activity18.RpgGame.Services.Implementation;
+// TODO: averigua por qué no se puede acceder a este namespace y sin embargo si puedes acceder al namespace Activity18.RpgGame.Services. La solución al segundo, la encontrarás en el archivo GlobalUsings.cs
+
+namespace Activity18.RpgGame.Startup;
+public static partial class ServiceCollectionExtensions
+{
+    //public static IServiceCollection RegisterServices(this IServiceCollection services) 
+    //{
+    //    throw new NotImplementedException(); 
+    // TODO: Registar aquí todos los servicios de la aplicación con el siguiente ejemplo: => services.AddTransient<IConnectionDataService, ConnectionFakeDataService>();
+    //}
+}
