0499400ea862bb585ef7db15713d5f8702338b19
Sapouu <saul2009.smg@gmail.com>
2024-03-05 20:48:16 +0100

    He hecho el register services

--- Activity18.RpgGame/Program.cs
+++ Activity18.RpgGame/Program.cs
@@ -1,12 +1,6 @@
 ﻿using Activity18.RpgGame.Services.Implementations;
 using Activity18.RpgGame.Startup;
 
-// TODO: Implementar Inyección de Dependencias en esta aplicación de consola siguiendo las instrucciones de la siguiente
-// guía: https://cord-lip-18a.notion.site/Implementaci-n-de-Inyecci-n-de-Dependencias-en-una-aplicaci-n-de-Consola-en-NET-fa56ff02288841339f8fc959402bcff8?pvs=4
-
-// TODO: El registro de Servicios no se hara aquí, se hará en un método estático de la clase ServiceCollectionExtensions
-// que se encuentra en el proyecto Activity18.RpgGame/Startup/ServiceCollectionExtensions.cs
-
 // TODO: Una vez hayas hecho la implementación de la inyección de dependencias, deberás de obtener una instancia de la
 // IRpgGame y llamar al método Start() para que la aplicación funcione correctamente. .
 
@@ -20,7 +14,7 @@ using System.Threading.Tasks;
 using IHost host = Host.CreateDefaultBuilder(args)
             .ConfigureServices((_, services) =>
             {
-                services.AddTransient<IGreetingService, GreetingService>();
+                services.RegisterServices();    
             })
             .Build();
 
--- Activity18.RpgGame/Startup/ServiceCollectionExtensions.cs
+++ Activity18.RpgGame/Startup/ServiceCollectionExtensions.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Activity18.RpgGame.Startup.ServiceCollectionExtensions;
-// TODO: necesitaras acceder al siguiente namespace para registrar los servicios: using Activity18.RpgGame.Services.Implementation;
-// TODO: averigua por qué no se puede acceder a este namespace y sin embargo si puedes acceder al namespace Activity18.RpgGame.Services. La solución al segundo, la encontrarás en el archivo GlobalUsings.cs
+using Activity18.RpgGame.Services.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Activity18.RpgGame.Startup;
 
 
 public static partial class ServiceCollectionExtensions
 {
- 
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddTransient<IGreetingService, GreetingService>();
+        services.AddTransient<IMageService, MageService>();
+        services.AddTransient<IWarriorService, Warrior>();
+        //services.AddTransient<IHeroGeneratorService, >();
+        //services.AddTransient<IHeroService, >();
+        //services.AddTransient<IRpgGameService, >();
+
+        return services;
+    }
+    
+    //TODO: Queda pendiente registrar el metodo start
 }
